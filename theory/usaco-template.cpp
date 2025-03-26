6a451709ee9a0c1a667f2de5bad196bfa4859559
Nicolas Arsenault <temporaryjustice@outlook.com>
2025-03-26 18:26:48 -0400

    Create usaco-template.cpp

---
+++ theory/usaco-template.cpp
@@ -0,0 +1,18 @@
+​​​​#include <iostream>
+​​​​#include <string>
+​​​​#include <vector>
+​​​​using namespace std;
+
+​​​​void setIO(string s){
+​​​​    ios_base::sync_with_stdio(0);
+​​​​    cin.tie(0);
+​​​​    freopen((s + ".in").c_str(), "r", stdin);
+​​​​    freopen((s + ".out").c_str(), "w", stdout);
+​​​​}
+
+​​​​int main(){
+​​​​    setIO("mixmilk");
+
+
+​​​​    return 0;
+​​​​}
